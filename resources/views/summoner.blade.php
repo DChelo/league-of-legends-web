7d8fe0cc4c1dedb757855451cef2c25180826556
Angelo <lucumichelo2003@gmail.com>
2023-09-01 12:52:56 -0500

    Agragar contenido

--- resources/views/summoner.blade.php
+++ resources/views/summoner.blade.php
@@ -6,7 +6,9 @@
 </head>
 <body>
     <h1>Summoner Information</h1>
-    <p>Summoner Name: {{ $data['name'] }}</p>
-    <p>Summoner Level: {{ $data['summonerLevel'] }}</p>
+    <p>Summoner Name: {{ $data['name'] }} Este es mi nombre de invocador</p>
+    <p>Summoner Level: {{ $data['summonerLevel'] }} Este es mi nivel de invocador
+
+    </p>
 </body>
 </html>
